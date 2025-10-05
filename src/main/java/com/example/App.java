fc06f096fc5b5e68ddbc0278587ee16ec8c8d6f3
Nisaa Dawi <wereen0909@gmail.com>
2025-10-06 00:54:59 +0800

    test ucp in java code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,16 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ucpbased.UCP;
+
+public class App {
+    public static void main(String[] args) {
+        UCP ucp = new UCP();
+        ucp.input_UUCW(0, 29, 0);
+        ucp.input_UAW(0, 0, 5);
+        ucp.input_TCF(3, 3, 3, 3, 0, 3, 3, 0, 3, 3, 3, 0, 3);
+        ucp.input_ECF(3, 3, 3, 3, 3, 3, 0, 0);
+
+        System.out.println(ucp.getUCP());
+        System.out.println(ucp.getPF());
     }
 }
